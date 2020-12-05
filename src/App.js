94dd0daefe209e921150803de79462fd6f40e6c3
huuthe1999 <55992625+huuthe1999@users.noreply.github.com>
2020-12-06 03:06:51 +0700

    Add image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Card, Chart, CountryPicker } from './components';
 import { fetchData } from './api';
 
 import './style.css';
+import imageCovid from './images/image.png';
 
 function App() {
     const [data, setData] = useState(null);
@@ -24,6 +25,7 @@ function App() {
     }
     return (
         <div className="container">
+            <img src={imageCovid} alt="COVID-19" className="image" />
             {data && <Card data={data} />}
             <CountryPicker handleChangeCountry={handleChangeCountry} />
             {data && <Chart data={data} country={country} />}
